dd1f81597a7f2ed9c545f8704f50560ec80a8753
Igor Patro <i.patro@wp.pl>
2020-11-15 11:17:07 +0100

    first test commit; testig eslint, lint-staged and husky

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  extends: [`airbnb`, `airbnb/hooks`, `react-app`, `prettier`],
+  globals: {
+    __PATH_PREFIX__: true,
+    document: true,
+    window: true,
+  },
+  rules: {
+    "react/jsx-filename-extension": [
+      1,
+      {
+        extensions: [".js"],
+      },
+    ],
+    "react/prop-types": 2,
+    semicolon: 0,
+    quotes: 0,
+    "linebreak-style": 0,
+    "import/no-unresolved": 0,
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        paths: ["./src"],
+      },
+    },
+  },
+}
